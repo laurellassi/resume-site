8eaa0eb6023dab8e0c812098dd7bf0324029b750
laurellassi <lassimol@myumanitoba.ca>
2025-03-06 09:01:26 -0600

    Updated Pelican settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Laurel Lassi'
 SITENAME = 'Laurel Lassi Resume'
-SITEURL = 'https://laurellassi.github.io/resume-site/'
+SITEURL = ''
 
 PATH = 'content'
 TIMEZONE = 'America/Winnipeg'
